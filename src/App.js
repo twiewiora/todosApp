8a30a980e5bf06a58a9f47cd7314595edc433a92
Gabriela Kowalik <gzmkowalik@gmail.com>
2018-04-14 20:07:54 +0200

    added buttons (prototype?)

--- src/App.js
+++ src/App.js
@@ -2,22 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 import TableEx from './TableExample';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-
+import {RaisedButton, TextField} from "material-ui";
 
 class App extends Component {
     state = {
         data: []
     };
 
+    addTask = function (e) {
+        let name = document.getElementById("taskName").value;
+
+        this.setState({data : [...this.state.data, name]});
+        e.preventDefault();
+    }
     render() {
         return (
             <div className="App">
                 <h1 className="title"> New task </h1>
-                <form>
-                    <input type="text" id="taskName" placeholder="Name"/><br/>
-                    <button onClick={(e) => {this.addTask(e)}}>Add</button><br/>
-                </form>
                 <MuiThemeProvider>
+                    <TextField id="taskName" hintText="name"/><br />
+                    <RaisedButton
+                        label="Add task"
+                        id="addButton"
+                        onClick={(e) => { this.addTask(e) }}
+                    />
                     <div className="App">
                         <TableEx
                             data={this.state.data}
@@ -28,12 +36,7 @@ class App extends Component {
         );
     }
 
-    addTask(e){
-        let name = document.getElementById("taskName").value;
 
-        this.setState({data : [...this.state.data, name]});
-        e.preventDefault();
-    }
 }
 
 export default App;
