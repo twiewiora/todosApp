ac41c4ace6d13a8466c7afafd27f742792812fc0
piotrgajdzica <piotr.gajdzica@wp.pl>
2018-04-17 16:12:17 +0200

    implemented most of the tests

--- src/test/todosWebApp/TaskServiceTest.java
+++ src/test/todosWebApp/TaskServiceTest.java
@@ -1,6 +1,5 @@
 package todosWebApp;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseOperation;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
@@ -13,16 +12,15 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import todosWebApp.controller.TaskController;
 import todosWebApp.persistence.model.Category;
 import todosWebApp.persistence.model.Task;
+import todosWebApp.persistence.service.CategoryService;
 import todosWebApp.persistence.service.TaskService;
 
-import java.io.IOException;
-import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(value = "file:webapp/WEB-INF/applicationContext.xml")
@@ -34,11 +32,44 @@ public class TaskServiceTest {
     @Autowired
     private TaskService taskService;
 
+    @Autowired
+    private CategoryService categoryService;
+
+    @Before
+    public void initDatabase(){
+        categoryService.createRootCategoryIfNotExists();
+
+
+        taskService.createTask("TitleTask_1", 1000L);
+        taskService.createTask("TitleTask_2", 1000L);
+
+        Category house = categoryService.createCategory("house");
+        Category tidying = categoryService.createCategory("tidying", house.getId());
+
+        taskService.createTask("cleaning room 1", tidying);
+
+    }
+
     @After
     public void cleanDatabase() {
         for( Task task : taskService.getAllTasks()){
             taskService.deleteTask(task.getId());
         }
+
+        deleteCategory(categoryService.getRootCategory());
+
+        for(Category category : categoryService.getAllCategories()){
+
+            categoryService.deleteCategory(category.getId());
+        }
+    }
+
+    private void deleteCategory(Category category){
+        for(Category child: categoryService.getChildren(category.getId())){
+            deleteCategory(child);
+        }
+
+        categoryService.deleteCategory(category.getId());
     }
 
     @Test
@@ -49,40 +80,94 @@ public class TaskServiceTest {
 
     @Test
     public void getTaskByIdTest() {
-        Task task = taskService.getTaskById(Long.valueOf(1));
-        assertEquals(Long.valueOf(1), task.getId());
+        Task inserted = taskService.createTask("work");
+        Task task = taskService.getTaskById(inserted.getId());
+        assertEquals(inserted.getId(), task.getId());
     }
 
     @Test
     public void getTaskByCategoryTest() {
-        List<Task> taskList = taskService.getTasksByCategory(Long.valueOf(1));
+        List<Task> taskList = taskService.getTasksByCategory(categoryService.getRootCategory().getId());
         assertEquals(true, taskList.stream().anyMatch(task -> task.getTitle().equals("TitleTask_1")));
     }
     //TODO implement tests
 
     @Test
-    public void getAllTasksTest(){}
+    public void getAllTasksTest(){
+        List<Task> tasks = taskService.getAllTasks();
+
+        assertNotEquals(tasks.get(0).getId(), tasks.get(1).getId());
+    }
 
     @Test
-    public void createTaskTest(){}
+    public void createTaskTest(){
+
+        Task t1 = taskService.createTask("work", 1000L);
+
+        assertEquals(t1.getCategory().getName(), "root");
+        assertEquals(t1.getDate(), Long.valueOf(1000L));
+        assertEquals(t1.getTitle(), "work");
+
+    }
 
     @Test
-    public void setTaskDoneTest(){}
+    public void setTaskDoneTest(){
+
+        Task t1 = taskService.createTask("work", 1000L);
+        assertEquals(t1.getDone(), false);
+
+        taskService.setDone(t1.getId(), true);
+        assertEquals(taskService.getTaskById(t1.getId()).getDone(), true);
+
+
+    }
 
     @Test
-    public void deleteTaskTest(){}
+    public void deleteTaskTest(){
+        Task t1 = taskService.createTask("work", 1000L);
+
+        taskService.deleteTask(t1.getId());
+        assertEquals(taskService.getTaskById(t1.getId()), null);
+
+    }
 
     @Test
     public void moveTaskTest(){}
 
     @Test
-    public void assignDateTest(){}
+    public void assignDateTest(){
+
+        Task t1 = taskService.createTask("work", 1000L);
+
+        assertEquals(t1.getDate(), Long.valueOf(1000L));
+
+        taskService.assignDate(t1.getId(), 2000L);
+
+        assertEquals(taskService.getTaskById(t1.getId()).getDate(), Long.valueOf(2000L));
+
+
+    }
 
     @Test
-    public void assignCategoryTest(){}
+    public void assignCategoryTest(){
+        Task t1 = taskService.createTask("work", 1000L);
+        Category c1 = categoryService.createCategory("job");
+
+        taskService.assignCategory(t1.getId(), c1.getId());
+
+        assertEquals(taskService.getTaskById(t1.getId()).getCategory().getName(), "job");
+
+    }
 
     @Test
-    public void getAllBaseCategoriesTest(){}
+    public void getAllBaseCategoriesTest(){
+
+        List<Category> categories = categoryService.getAllBaseCategories();
+
+        assertEquals(categories.size(), 1);
+        assertEquals(categories.get(0).getName(), "house");
+
+    }
 
     @Test
     public void  getAllCategoriesTest(){}
