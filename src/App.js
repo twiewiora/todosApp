ff109d78f62ac241b40d0a56275f91a585fcd3d7
Gabriela Kowalik <gzmkowalik@gmail.com>
2018-04-29 22:43:12 +0200

    added subpage calendar

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import './Styles/App.css';
 import MaterialAll from './MaterialUIs';
 import MenuBase from './Menu'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {FontIcon, RaisedButton} from "material-ui";
+import Calendar from "./Calendar";
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
+const App = (props) =>
+    <div className="App">
                 <MuiThemeProvider>
                     <div className="App">
                         <MenuBase/>
+                        <RaisedButton className="calendarButton"
+                            target="_blank"
+                            label="Calendar Mode"
+                            onClick={e=>props.pager.push(Calendar)}
+                        />
                         <MaterialAll/>
                     </div>
                 </MuiThemeProvider>
             </div>
-        );
-    }
 
-
-}
 
 export default App;
