0aa2d75289ab577d9a2fb0596eb3976bc8f72852
Anna Alberska <anna.alberska@wp.pl>
2018-05-07 18:20:31 +0200

    fixed calendar assign date

--- front/src/Utils/ArrayFunctions.js
+++ front/src/Utils/ArrayFunctions.js
@@ -2,7 +2,7 @@ export function removeIndex(array, index) {
     let newArray = [];
     for (let i = 0; i < array.length; i++){
         if (i !== index){
-            newArray.push(array[index]);
+            newArray.push(array[i]);
         }
     }
     return newArray;
