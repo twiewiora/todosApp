b0c50e2f70fec96e9320a2ace04f8fe5ad93a1c4
acofalka <a.cofalka@gmail.com>
2018-05-14 02:48:06 +0200

    Added GUI test

--- src/MaterialUIs.jsx
+++ src/MaterialUIs.jsx
@@ -126,7 +126,7 @@ class MaterialUIs extends Component {
                         label="Add task"
                         id="addButton"
                         onClick={(e) => {
-                            this.addTask(e)
+                            this.props.addTask(e)
                         }}
                     /><br/><br/>
                     <Table
