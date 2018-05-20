232c9ed6c1d2e9d75841198da84ebc9ae9937485
Magdalena Nowak <madzia.nowak222@poczta.fm>
2018-05-20 23:14:51 +0200

    some fixes and new functionality - deleting categories

--- front/src/Menu.jsx
+++ front/src/Menu.jsx
@@ -3,8 +3,9 @@ import MenuIcon from "material-ui/svg-icons/navigation/menu"
 import ArrowIcon from "material-ui/svg-icons/navigation/arrow-back"
 import './Styles/App.css'
 import './Styles/Menu.css'
-import {addCategoryRequest, getAllCategories, getSubcategories} from './Requests/Requests'
-import {FloatingActionButton, RaisedButton, TextField} from "material-ui";
+import TrashIcon from "material-ui/svg-icons/action/delete";
+import {addCategoryRequest, deleteCategoryRequest, getAllCategories, getSubcategories} from './Requests/Requests'
+import {RaisedButton, TextField} from "material-ui";
 import Category from "./Category/Category";
 
 class Menu extends Component {
@@ -12,54 +13,100 @@ class Menu extends Component {
         super(props);
 
         this.state = {
-            data : getAllCategories(),
             currentCategory: new Category("Base categories", 1, null),
-            childrenCurrentCategory: getSubcategories(new Category("root", 1, null))
+            childrenCurrentCategory: [],
+            trashesVisibility: false
         };
+    }
 
-        setTimeout(function() {
-            let temp = this.state.data;
-            temp.unshift(new Category("None", 1, null));
-            this.props.setCurrentCategories(temp);
-        }.bind(this), 1000);
-
+    componentDidMount() {
+        getAllCategories()
+            .then(childrenCurrentCategory => {
+                console.log('Show categories', childrenCurrentCategory);
+                this.setState({childrenCurrentCategory});
+                this.props.setCurrentCategories(childrenCurrentCategory);
+            });
     }
 
     filterBackwards(e){
-        let currentCategoryID = this.state.currentCategory.getParentID();
-        let currentCategory = new Category("Base categories", 1, null);
-        let temp = new Category("None", 1, null);
-        let categoriesToDisplayInTasks = [];
+        e.stopPropagation();
 
-        if(currentCategoryID !== 1){
-            for(let i = 0; i < this.state.data.length; i++){
-                if(this.state.data[i].getID() === currentCategoryID){
-                    currentCategory = this.state.data[i];
-                    temp = this.state.data[i];
-                    break;
+        getAllCategories().then(data => {
+            let currentCategoryID = this.state.currentCategory.getParentID();
+            let currentCategory = new Category("Base categories", 1, null);
+            let temp = new Category("None", 1, null);
+            let categoriesToDisplayInTasks = [];
+
+            if (currentCategoryID !== 1) {
+                for (let i = 0; i < data.length; i++) {
+                    if (data[i].getID() === currentCategoryID) {
+                        currentCategory = data[i];
+                        temp = data[i];
+                        break;
+                    }
                 }
             }
-        }
-        categoriesToDisplayInTasks.push(temp);
+            categoriesToDisplayInTasks.push(temp);
 
-        if (currentCategoryID === null){
-            this.props.toggleMenu();
-        } else {
+            if (currentCategoryID === null) {
+                this.props.toggleMenu();
+            } else {
+                let categories = [];
+                for (let i = 0; i < data.length; i++) {
+                    if (data[i].getParentID() === currentCategoryID) {
+                        categories.push(data[i]);
+                        categoriesToDisplayInTasks.push(data[i]);
+                    }
+                }
+
+                let j = 1;
+                while (j !== categoriesToDisplayInTasks.length) {
+                    let cID = categoriesToDisplayInTasks[j].getID();
+
+                    for (let k = 0; k < data.length; k++) {
+                        if (data[k].getParentID() === cID) {
+                            categoriesToDisplayInTasks.push(data[k]);
+                        }
+                    }
+                    j += 1;
+                }
+
+                this.setState({
+                    currentCategory: currentCategory,
+                    childrenCurrentCategory: categories
+                });
+
+                this.props.setCurrentCategories(categoriesToDisplayInTasks);
+                this.props.setSelectedCategory(currentCategoryID);
+
+
+            }
+        });
+    }
+
+    filterCategories(e, i) {
+        getAllCategories().then(data => {
             let categories = [];
-            for(let i = 0; i < this.state.data.length; i++){
-                if(this.state.data[i].getParentID() === currentCategoryID){
-                    categories.push(this.state.data[i]);
-                    categoriesToDisplayInTasks.push(this.state.data[i]);
+            let categoriesToDisplayInTasks = [];
+            let length = data.length;
+            let currentCategory = this.state.childrenCurrentCategory[i];
+            let currentCategoryID = currentCategory.getID();
+            categoriesToDisplayInTasks.push(currentCategory);
+
+            for (let j = 0; j < length; j++) {
+                if (data[j].getParentID() === currentCategoryID) {
+                    categories.push(data[j]);
+                    categoriesToDisplayInTasks.push(data[j]);
                 }
             }
 
             let j = 1;
-            while(j !== categoriesToDisplayInTasks.length){
+            while (j !== categoriesToDisplayInTasks.length) {
                 let cID = categoriesToDisplayInTasks[j].getID();
 
-                for(let k = 0; k < this.state.data.length; k++){
-                    if(this.state.data[k].getParentID() === cID){
-                        categoriesToDisplayInTasks.push(this.state.data[k]);
+                for (let k = 0; k < length; k++) {
+                    if (data[k].getParentID() === cID) {
+                        categoriesToDisplayInTasks.push(data[k]);
                     }
                 }
                 j += 1;
@@ -72,46 +119,7 @@ class Menu extends Component {
 
             this.props.setCurrentCategories(categoriesToDisplayInTasks);
             this.props.setSelectedCategory(currentCategoryID);
-
-
-        }
-        e.stopPropagation();
-    }
-
-    filterCategories(e, i) {
-        let categories = [];
-        let categoriesToDisplayInTasks = [];
-        let length = this.state.data.length;
-        let currentCategory = this.state.childrenCurrentCategory[i];
-        let currentCategoryID = currentCategory.getID();
-        categoriesToDisplayInTasks.push(currentCategory);
-
-        for(let j = 0; j < length; j++){
-            if(this.state.data[j].getParentID() === currentCategoryID){
-                categories.push(this.state.data[j]);
-                categoriesToDisplayInTasks.push(this.state.data[j]);
-            }
-        }
-
-        let j = 1;
-        while(j !== categoriesToDisplayInTasks.length){
-            let cID = categoriesToDisplayInTasks[j].getID();
-
-            for(let k = 0; k < length; k++){
-                if(this.state.data[k].getParentID() === cID){
-                    categoriesToDisplayInTasks.push(this.state.data[k]);
-                }
-            }
-            j += 1;
-        }
-
-        this.setState({
-            currentCategory: currentCategory,
-            childrenCurrentCategory: categories
         });
-
-        this.props.setCurrentCategories(categoriesToDisplayInTasks);
-        this.props.setSelectedCategory(currentCategoryID);
     }
 
     addCategory() {
@@ -119,13 +127,41 @@ class Menu extends Component {
         console.log(name);
 
         if (name !== ""){
-            let newTask = addCategoryRequest(name, this.state.parentCategory.getID());
-            let temp = this.state.data;
-            temp.push(newTask);
-            this.setState({data : temp});
+            let newTask = addCategoryRequest(name, this.state.currentCategory.getID());
+            this.setState({
+                childrenCurrentCategory: [...this.state.childrenCurrentCategory, newTask]
+            });
             document.getElementById('taskName').value = "";
             document.getElementById('taskName').hintText = "Add a category";
         }
+
+    }
+
+    deleteCategory(object, index) {
+        //let name = document.getElementById("categoryName").value;
+
+        //TODO
+        //if (name !== "") {
+
+        //let taskToDelete = deleteCategoryRequest(name, this.state.name.getID());
+        //let temp = this.state.data;
+        //temp.push(newTask);
+        //this.setState({data : temp});
+        //document.getElementById('taskName').value = "";
+        //document.getElementById('taskName').hintText = "Add a category";
+        //}
+
+        deleteCategoryRequest(this.state.childrenCurrentCategory[index])
+            .then(() => getSubcategories(this.state.currentCategory))
+            .then(childrenCurrentCategory => {
+                this.setState({childrenCurrentCategory});
+            });
+    }
+
+    showTrashes() {
+        this.setState({
+            trashesVisibility: !this.state.trashesVisibility,
+        });
     }
 
     render() {
@@ -141,12 +177,27 @@ class Menu extends Component {
                            className="BackIcon" onClick={(e) => {this.filterBackwards(e)}}/>
                 <h1 className="title">{this.state.currentCategory.getName()}</h1>
                 {this.state.childrenCurrentCategory.map((object, index) =>
-                    <li key={`item-${index}`}
-                        onClick={(e) => { this.filterCategories(e, index) }}>
-                        <h2>{object.getName()}</h2>
+                    <li key={`item-${index}`}>
+                        {
+                            this.state.trashesVisibility
+                                ? (<TrashIcon id="trashIcon"
+                                              style={{
+                                                  float: 'right',
+                                                  margin: '5px 15px 5px 5px',
+                                                  color: 'white',
+                                                  cursor: 'pointer'
+                                              }}
+                                              onClick={() => {
+                                                  this.deleteCategory(object, index)
+                                              }}/>)
+                                : null
+                        }
+
+                        <h2 onClick={(e) => {
+                            this.filterCategories(e, index)
+                        }}>{object.getName()}</h2>
                     </li>)}
                 <TextField id="categoryName" style={{margin: '0 8px'}} hintStyle={{color: '#bababa'}} inputStyle={{color: 'white'}} hintText="Add a category"/><br />
-                {/*TODO change to FloatingActionButton*/}
                 <RaisedButton
                     label="Add Category"
                     style={{margin: '0 auto'}}
@@ -154,6 +205,14 @@ class Menu extends Component {
                     onClick={(e) => {
                         this.addCategory(e)
                     }} />
+                <br/><br/>
+                <RaisedButton
+                    label="Delete Category"
+                    style={{margin: '0 auto'}}
+                    id="deleteButton"
+                    onClick={(e) => {
+                        this.showTrashes(e)
+                    }}/>
             </div>
         );
     }
